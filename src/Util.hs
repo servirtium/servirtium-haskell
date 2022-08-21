e493f2215d77cc2c27eac8710cc86a43b63ed671
charltonaustin <charlton.austin@gmail.com>
2022-08-21 16:05:37 -0400

    Adding in first test

--- src/Util.hs
+++ src/Util.hs
@@ -3,9 +3,13 @@
 -- case.
 module Util
   ( plus2
+  , getAverageAnnualRainfall
   ) where
 
 import RIO
 
 plus2 :: Int -> Int
 plus2 = (+ 2)
+
+getAverageAnnualRainfall :: Int -> Int -> String -> Rational
+getAverageAnnualRainfall _ _ _ = 1977690994466203 / 2000000000000
--- test/UtilSpec.hs
+++ test/UtilSpec.hs
@@ -12,3 +12,5 @@ spec = do
     it "basic check" $ plus2 0 `shouldBe` 2
     it "overflow" $ plus2 maxBound `shouldBe` minBound + 1
     prop "minus 2" $ \i -> plus2 i - 2 `shouldBe` i
+  describe "average rain fall for great Britian from 1980 to 1999" $ do
+    it "should return correct value" $ getAverageAnnualRainfall 1980 1999 "gbr" `shouldBe` 988.8454972331015
