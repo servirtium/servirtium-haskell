b73e045f3bf012e930e1a9298297daf0c02ee850
charltonaustin <charlton.austin@gmail.com>
2022-09-18 15:37:41 -0400

    Getting the average from the list

--- src/scratch.hs
+++ src/scratch.hs
@@ -2,9 +2,13 @@
 import Network.Wreq               as WR
 import Text.HTML.TagSoup          as TS
 import Data.ByteString.Lazy.Char8 as Char8
-import Prelude as P
+import Data.List                  as L
+import Prelude                    as P
 r <- get "https://servirtium.github.io/worldbank-climate-recordings/climateweb/rest/v1/country/annualavg/pr/1980/1999/gbr.xml"
 b =  Char8.unpack (r ^. responseBody)
 tags = parseTags b
 values = P.map fromTagText (P.map (P.!!1) (partitions (isTagOpenName "double") tags))
 doubleValues = P.map P.read values :: [Double]
+doubleLength = L.genericLength doubleValues
+doubleSum = P.realToFrac (P.sum(doubleValues))
+if doubleLength > 0 then doubleSum / doubleLength else 0
